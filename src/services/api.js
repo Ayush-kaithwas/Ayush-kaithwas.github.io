bb5d1e349fd31ad45709357fca0de9098a466529
Ayush-kaithwas <ayushharrysudha@gmail.com>
2026-02-09 13:38:03 +0530

    initial commit

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+export const fetchPortfolioData = async () => {
+  try {
+    const response = await fetch('/data.json');
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching portfolio data:", error);
+    return null;
+  }
+};
